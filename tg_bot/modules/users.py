12b569f375d140982e1e1bf4b0ea738c8a62d499
nwwr9235 <nwwr9235@gmail.com>
2026-03-13 04:03:22 +0300

    Update users.py

--- tg_bot/modules/users.py
+++ tg_bot/modules/users.py
@@ -17,7 +17,7 @@ CHAT_GROUP = 10
 
 
 def get_user_id(username):
-    # ensure valid userid
+    # تأكد من صحة معرف المستخدم
     if len(username) <= 5:
         return None
 
@@ -43,7 +43,7 @@ def get_user_id(username):
                 if excp.message == 'Chat not found':
                     pass
                 else:
-                    LOGGER.exception("Error extracting user ID")
+                    LOGGER.exception("خطأ في استخراج معرف المستخدم")
 
     return None
 
@@ -60,9 +60,8 @@ def broadcast(bot: Bot, update: Update):
                 sleep(0.1)
             except TelegramError:
                 failed += 1
-                LOGGER.warning("Couldn't send broadcast to %s, group name %s", str(chat.chat_id), str(chat.chat_name))                
-        update.effective_message.reply_text("Broadcast complete. {} groups failed to receive the message, probably "
-                                            "due to being kicked.".format(failed))
+                LOGGER.warning("تعذر إرسال البث إلى %s، اسم المجموعة %s", str(chat.chat_id), str(chat.chat_name))
+        update.effective_message.reply_text("تم البث بنجاح. {} مجموعة فشلت في استلام الرسالة، ربما بسبب الطرد.".format(failed))
 
 
 @run_async
@@ -76,19 +75,17 @@ def userbroadcast(bot: Bot, update: Update):
             try:
                 bot.sendMessage(int(user.user_id), to_send[1])
                 success += 1
-                LOGGER.warning("Sent broadcast to %s, username %s, Count: %s", str(user.user_id), str(user.username), str(success))
+                LOGGER.warning("تم إرسال البث إلى %s، اسم المستخدم %s، العدد: %s", str(user.user_id), str(user.username), str(success))
                 sleep(0.5)
             except TelegramError:
                 failed += 1
-                # LOGGER.warning("Couldn't send broadcast to %s, username %s", str(user.user_id), str(user.username))
-        update.effective_message.reply_text("Broadcast complete.\n{} users failed\n{} users received".format(failed, success))
-
+        update.effective_message.reply_text("تم البث بنجاح.\n{} مستخدم فشلوا\n{} مستخدم استلموا".format(failed, success))
 
 
 @run_async
 def log_user(bot: Bot, update: Update):
-    chat = update.effective_chat  # type: Optional[Chat]
-    msg = update.effective_message  # type: Optional[Message]
+    chat = update.effective_chat
+    msg = update.effective_message
 
     sql.update_user(msg.from_user.id,
                     msg.from_user.username,
@@ -109,30 +106,31 @@ def log_user(bot: Bot, update: Update):
 @run_async
 def chats(bot: Bot, update: Update):
     all_chats = sql.get_all_chats() or []
-    chatfile = 'List of chats.\n'
+    chatfile = 'قائمة الدردشات.\n'
     for chat in all_chats:
         chatfile += "{} - ({})\n".format(chat.chat_name, chat.chat_id)
 
     with BytesIO(str.encode(chatfile)) as output:
         output.name = "chatlist.txt"
         update.effective_message.reply_document(document=output, filename="chatlist.txt",
-                                                caption="Here is the list of chats in my database.")
+                                                caption="هذه قائمة الدردشات في قاعدة بياناتي.")
+
 
 @run_async
 def chat_checker(bot: Bot, update: Update):
-  if update.effective_message.chat.get_member(bot.id).can_send_messages == False:
-    bot.leaveChat(update.effective_message.chat.id)
+    if update.effective_message.chat.get_member(bot.id).can_send_messages == False:
+        bot.leaveChat(update.effective_message.chat.id)
 
 
 def __user_info__(user_id):
     if user_id == dispatcher.bot.id:
-        return """I've seen them in... Wow. Are they stalking me? They're in all the same places I am... oh. It's me."""
+        return """لقد رأيتهم في... واو. هل يتتبعونني؟ إنهم في كل الأماكن التي أتواجد فيها... أوه، إنه أنا."""
     num_chats = sql.get_user_num_chats(user_id)
-    return """<code>{}</code> ചാറ്റുകളിൽ ഇയാളെ ഞാൻ കണ്ടിട്ടുണ്ട്..""".format(num_chats)
+    return """<code>{}</code> دردشة رأيت هذا المستخدم فيها.""".format(num_chats)
 
 
 def __stats__():
-    return "{} users, across {} chats".format(sql.num_users(), sql.num_chats())
+    return "{} مستخدم، عبر {} دردشة".format(sql.num_users(), sql.num_chats())
 
 
 def __gdpr__(user_id):
@@ -143,9 +141,10 @@ def __migrate__(old_chat_id, new_chat_id):
     sql.migrate_chat(old_chat_id, new_chat_id)
 
 
-__help__ = ""  # no help string
+# ================== المساعدة ==================
+__help__ = ""  # لا يوجد نص مساعدة لهذه الوحدة
 
-__mod_name__ = "Users"
+__mod_name__ = "المستخدمون"
 
 BROADCAST_HANDLER = CommandHandler("broadcast", broadcast, filters=Filters.user(OWNER_ID))
 USER_HANDLER = MessageHandler(Filters.all & Filters.group, log_user)
@@ -153,8 +152,17 @@ USER_BROADCAST_HANDLER = CommandHandler("userbroadcast", userbroadcast, filters=
 CHATLIST_HANDLER = CommandHandler("chatlist", chats, filters=CustomFilters.sudo_filter)
 CHAT_CHECKER_HANDLER = MessageHandler(Filters.all & Filters.group, chat_checker)
 
+# معالجات الأوامر العربية
+BROADCAST_AR_HANDLER = CommandHandler("بث", broadcast, filters=Filters.user(OWNER_ID))
+USERBROADCAST_AR_HANDLER = CommandHandler("بث_مستخدمين", userbroadcast, filters=Filters.user(OWNER_ID))
+CHATLIST_AR_HANDLER = CommandHandler("قائمة_الدردشات", chats, filters=CustomFilters.sudo_filter)
+
 dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
 dispatcher.add_handler(BROADCAST_HANDLER)
 dispatcher.add_handler(USER_BROADCAST_HANDLER)
 dispatcher.add_handler(CHATLIST_HANDLER)
 dispatcher.add_handler(CHAT_CHECKER_HANDLER, CHAT_GROUP)
+
+dispatcher.add_handler(BROADCAST_AR_HANDLER)
+dispatcher.add_handler(USERBROADCAST_AR_HANDLER)
+dispatcher.add_handler(CHATLIST_AR_HANDLER)
