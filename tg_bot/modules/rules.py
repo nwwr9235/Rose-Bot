f537c008de9a9abf7b4ae3fc828171be5eaf84b6
nwwr9235 <nwwr9235@gmail.com>
2026-03-13 04:04:32 +0300

    Update rules.py

--- tg_bot/modules/rules.py
+++ tg_bot/modules/rules.py
@@ -18,53 +18,51 @@ def get_rules(bot: Bot, update: Update):
     send_rules(update, chat_id)
 
 
-# Do not async - not from a handler
+# ليست async - ليست من معالج
 def send_rules(update, chat_id, from_pm=False):
     bot = dispatcher.bot
-    user = update.effective_user  # type: Optional[User]
+    user = update.effective_user
     try:
         chat = bot.get_chat(chat_id)
     except BadRequest as excp:
         if excp.message == "Chat not found" and from_pm:
-            bot.send_message(user.id, "The rules shortcut for this chat hasn't been set properly! Ask admins to "
-                                      "fix this.")
+            bot.send_message(user.id, "اختصار القواعد لهذه الدردشة لم يتم تعيينه بشكل صحيح! اطلب من المشرفين إصلاح هذا.")
             return
         else:
             raise
 
     rules = sql.get_rules(chat_id)
-    text = "The rules for *{}* are:\n\n{}".format(escape_markdown(chat.title), rules)
+    text = "قواعد *{}* هي:\n\n{}".format(escape_markdown(chat.title), rules)
 
     if from_pm and rules:
         bot.send_message(user.id, text, parse_mode=ParseMode.MARKDOWN)
     elif from_pm:
-        bot.send_message(user.id, "The group admins haven't set any rules for this chat yet. "
-                                  "This probably doesn't mean it's lawless though...!")
+        bot.send_message(user.id, "مشرفو المجموعة لم يضعوا أي قواعد لهذه الدردشة بعد. هذا لا يعني أنها خالية من القوانين...!")
     elif rules:
-        update.effective_message.reply_text("Contact me in PM to get this group's rules.",
+        update.effective_message.reply_text("تواصل معي في الخاص للحصول على قواعد هذه المجموعة.",
                                             reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="Rules",
+                                                [[InlineKeyboardButton(text="القواعد",
                                                                        url="t.me/{}?start={}".format(bot.username,
                                                                                                      chat_id))]]))
     else:
-        update.effective_message.reply_text("The group admins haven't set any rules for this chat yet. "
-                                            "This probably doesn't mean it's lawless though...!")
+        update.effective_message.reply_text("مشرفو المجموعة لم يضعوا أي قواعد لهذه الدردشة بعد. "
+                                            "هذا لا يعني أنها خالية من القوانين...!")
 
 
 @run_async
 @user_admin
 def set_rules(bot: Bot, update: Update):
     chat_id = update.effective_chat.id
-    msg = update.effective_message  # type: Optional[Message]
+    msg = update.effective_message
     raw_text = msg.text
-    args = raw_text.split(None, 1)  # use python's maxsplit to separate cmd and args
+    args = raw_text.split(None, 1)  # استخدم maxsplit في بايثون لفصل الأمر والوسائط
     if len(args) == 2:
         txt = args[1]
-        offset = len(txt) - len(raw_text)  # set correct offset relative to command
+        offset = len(txt) - len(raw_text)  # تعيين الإزاحة الصحيحة بالنسبة للأمر
         markdown_rules = markdown_parser(txt, entities=msg.parse_entities(), offset=offset)
 
         sql.set_rules(chat_id, markdown_rules)
-        update.effective_message.reply_text("Successfully set rules for this group.")
+        update.effective_message.reply_text("تم تعيين قواعد هذه المجموعة بنجاح.")
 
 
 @run_async
@@ -72,15 +70,15 @@ def set_rules(bot: Bot, update: Update):
 def clear_rules(bot: Bot, update: Update):
     chat_id = update.effective_chat.id
     sql.set_rules(chat_id, "")
-    update.effective_message.reply_text("Successfully cleared rules!")
+    update.effective_message.reply_text("تم مسح القواعد بنجاح!")
 
 
 def __stats__():
-    return "{} chats have rules set.".format(sql.num_chats())
+    return "{} دردشة لديها قواعد محددة.".format(sql.num_chats())
 
 
 def __import_data__(chat_id, data):
-    # set chat rules
+    # تعيين قواعد الدردشة
     rules = data.get('info', {}).get('rules', "")
     sql.set_rules(chat_id, rules)
 
@@ -90,23 +88,38 @@ def __migrate__(old_chat_id, new_chat_id):
 
 
 def __chat_settings__(chat_id, user_id):
-    return "This chat has had it's rules set: `{}`".format(bool(sql.get_rules(chat_id)))
+    return "هذه الدردشة لديها قواعد محددة: `{}`".format(bool(sql.get_rules(chat_id)))
 
 
+# ================== المساعدة ==================
 __help__ = """
- - /rules: get the rules for this chat.
+- /rules: الحصول على قواعد هذه الدردشة.
 
-*Admin only:*
- - /setrules <your rules here>: set the rules for this chat.
- - /clearrules: clear the rules for this chat.
+*للمشرفين فقط:*
+- /setrules <قواعدك هنا>: تعيين قواعد هذه الدردشة.
+- /clearrules: مسح قواعد هذه الدردشة.
+
+*الأوامر العربية (بدون /):*
+القواعد: عرض قواعد المجموعة
+تعيين قواعد <النص>: تعيين قواعد المجموعة
+مسح قواعد: مسح قواعد المجموعة
 """
 
-__mod_name__ = "Rules"
+__mod_name__ = "القواعد"
 
 GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
 SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
 RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules, filters=Filters.group)
 
+# معالجات الأوامر العربية
+GET_RULES_AR_HANDLER = CommandHandler("القواعد", get_rules, filters=Filters.group)
+SET_RULES_AR_HANDLER = CommandHandler("تعيين قواعد", set_rules, filters=Filters.group)
+CLEAR_RULES_AR_HANDLER = CommandHandler("مسح قواعد", clear_rules, filters=Filters.group)
+
 dispatcher.add_handler(GET_RULES_HANDLER)
 dispatcher.add_handler(SET_RULES_HANDLER)
 dispatcher.add_handler(RESET_RULES_HANDLER)
+
+dispatcher.add_handler(GET_RULES_AR_HANDLER)
+dispatcher.add_handler(SET_RULES_AR_HANDLER)
+dispatcher.add_handler(CLEAR_RULES_AR_HANDLER)
