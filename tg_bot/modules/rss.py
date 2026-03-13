eddd9d6793fa4fb57af1f9106a074eda60536a79
nwwr9235 <nwwr9235@gmail.com>
2026-03-13 04:06:32 +0300

    Update rss.py

--- tg_bot/modules/rss.py
+++ tg_bot/modules/rss.py
@@ -3,7 +3,7 @@ import re
 
 from feedparser import parse
 from telegram import ParseMode, constants
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, MessageHandler, Filters
 
 from tg_bot import dispatcher, updater
 from tg_bot.modules.helper_funcs.chat_status import user_admin
@@ -18,26 +18,26 @@ def show_url(bot, update, args):
         link_processed = parse(tg_feed_link)
 
         if link_processed.bozo == 0:
-            feed_title = link_processed.feed.get("title", default="Unknown")
+            feed_title = link_processed.feed.get("title", default="غير معروف")
             feed_description = "<i>{}</i>".format(
-                re.sub('<[^<]+?>', '', link_processed.feed.get("description", default="Unknown")))
-            feed_link = link_processed.feed.get("link", default="Unknown")
+                re.sub('<[^<]+?>', '', link_processed.feed.get("description", default="غير معروف")))
+            feed_link = link_processed.feed.get("link", default="غير معروف")
 
-            feed_message = "<b>Feed Title:</b> \n{}" \
-                           "\n\n<b>Feed Description:</b> \n{}" \
-                           "\n\n<b>Feed Link:</b> \n{}".format(html.escape(feed_title),
+            feed_message = "<b>عنوان التغذية:</b> \n{}" \
+                           "\n\n<b>وصف التغذية:</b> \n{}" \
+                           "\n\n<b>رابط التغذية:</b> \n{}".format(html.escape(feed_title),
                                                                feed_description,
                                                                html.escape(feed_link))
 
             if len(link_processed.entries) >= 1:
-                entry_title = link_processed.entries[0].get("title", default="Unknown")
+                entry_title = link_processed.entries[0].get("title", default="غير معروف")
                 entry_description = "<i>{}</i>".format(
-                    re.sub('<[^<]+?>', '', link_processed.entries[0].get("description", default="Unknown")))
-                entry_link = link_processed.entries[0].get("link", default="Unknown")
+                    re.sub('<[^<]+?>', '', link_processed.entries[0].get("description", default="غير معروف")))
+                entry_link = link_processed.entries[0].get("link", default="غير معروف")
 
-                entry_message = "\n\n<b>Entry Title:</b> \n{}" \
-                                "\n\n<b>Entry Description:</b> \n{}" \
-                                "\n\n<b>Entry Link:</b> \n{}".format(html.escape(entry_title),
+                entry_message = "\n\n<b>عنوان المدخلة:</b> \n{}" \
+                                "\n\n<b>وصف المدخلة:</b> \n{}" \
+                                "\n\n<b>رابط المدخلة:</b> \n{}".format(html.escape(entry_title),
                                                                      entry_description,
                                                                      html.escape(entry_link))
                 final_message = feed_message + entry_message
@@ -46,9 +46,9 @@ def show_url(bot, update, args):
             else:
                 bot.send_message(chat_id=tg_chat_id, text=feed_message, parse_mode=ParseMode.HTML)
         else:
-            update.effective_message.reply_text("This link is not an RSS Feed link")
+            update.effective_message.reply_text("هذا الرابط ليس رابط تغذية RSS")
     else:
-        update.effective_message.reply_text("URL missing")
+        update.effective_message.reply_text("الرابط مفقود")
 
 
 def list_urls(bot, update):
@@ -56,19 +56,17 @@ def list_urls(bot, update):
 
     user_data = sql.get_urls(tg_chat_id)
 
-    # this loops gets every link from the DB based on the filter above and appends it to the list
     links_list = [row.feed_link for row in user_data]
 
     final_content = "\n\n".join(links_list)
 
-    # check if the length of the message is too long to be posted in 1 chat bubble
     if len(final_content) == 0:
-        bot.send_message(chat_id=tg_chat_id, text="This chat is not subscribed to any links")
+        bot.send_message(chat_id=tg_chat_id, text="هذه الدردشة غير مشتركة في أي روابط")
     elif len(final_content) <= constants.MAX_MESSAGE_LENGTH:
-        bot.send_message(chat_id=tg_chat_id, text="This chat is subscribed to the following links:\n" + final_content)
+        bot.send_message(chat_id=tg_chat_id, text="هذه الدردشة مشتركة في الروابط التالية:\n" + final_content)
     else:
         bot.send_message(chat_id=tg_chat_id, parse_mode=ParseMode.HTML,
-                         text="<b>Warning:</b> The message is too long to be sent")
+                         text="<b>تحذير:</b> الرسالة طويلة جداً ولا يمكن إرسالها")
 
 
 @user_admin
@@ -82,27 +80,24 @@ def add_url(bot, update, args):
 
         link_processed = parse(tg_feed_link)
 
-        # check if link is a valid RSS Feed link
         if link_processed.bozo == 0:
             if len(link_processed.entries[0]) >= 1:
                 tg_old_entry_link = link_processed.entries[0].link
             else:
                 tg_old_entry_link = ""
 
-            # gather the row which contains exactly that telegram group ID and link for later comparison
             row = sql.check_url_availability(tg_chat_id, tg_feed_link)
 
-            # check if there's an entry already added to DB by the same user in the same group with the same link
             if row:
-                update.effective_message.reply_text("This URL has already been added")
+                update.effective_message.reply_text("هذا الرابط تمت إضافته بالفعل")
             else:
                 sql.add_url(tg_chat_id, tg_feed_link, tg_old_entry_link)
 
-                update.effective_message.reply_text("Added URL to subscription")
+                update.effective_message.reply_text("تمت إضافة الرابط إلى الاشتراكات")
         else:
-            update.effective_message.reply_text("This link is not an RSS Feed link")
+            update.effective_message.reply_text("هذا الرابط ليس رابط تغذية RSS")
     else:
-        update.effective_message.reply_text("URL missing")
+        update.effective_message.reply_text("الرابط مفقود")
 
 
 @user_admin
@@ -120,19 +115,18 @@ def remove_url(bot, update, args):
             if user_data:
                 sql.remove_url(tg_chat_id, tg_feed_link)
 
-                update.effective_message.reply_text("Removed URL from subscription")
+                update.effective_message.reply_text("تمت إزالة الرابط من الاشتراكات")
             else:
-                update.effective_message.reply_text("You haven't subscribed to this URL yet")
+                update.effective_message.reply_text("لم تشترك في هذا الرابط بعد")
         else:
-            update.effective_message.reply_text("This link is not an RSS Feed link")
+            update.effective_message.reply_text("هذا الرابط ليس رابط تغذية RSS")
     else:
-        update.effective_message.reply_text("URL missing")
+        update.effective_message.reply_text("الرابط مفقود")
 
 
 def rss_update(bot, job):
     user_data = sql.get_all()
 
-    # this loop checks for every row in the DB
     for row in user_data:
         row_id = row.id
         tg_chat_id = row.chat_id
@@ -145,30 +139,26 @@ def rss_update(bot, job):
         new_entry_links = []
         new_entry_titles = []
 
-        # this loop checks for every entry from the RSS Feed link from the DB row
         for entry in feed_processed.entries:
-            # check if there are any new updates to the RSS Feed from the old entry
             if entry.link != tg_old_entry_link:
                 new_entry_links.append(entry.link)
                 new_entry_titles.append(entry.title)
             else:
                 break
 
-        # check if there's any new entries queued from the last check
         if new_entry_links:
             sql.update_url(row_id, new_entry_links)
         else:
             pass
 
         if len(new_entry_links) < 5:
-            # this loop sends every new update to each user from each group based on the DB entries
             for link, title in zip(reversed(new_entry_links), reversed(new_entry_titles)):
                 final_message = "<b>{}</b>\n\n{}".format(html.escape(title), html.escape(link))
 
                 if len(final_message) <= constants.MAX_MESSAGE_LENGTH:
                     bot.send_message(chat_id=tg_chat_id, text=final_message, parse_mode=ParseMode.HTML)
                 else:
-                    bot.send_message(chat_id=tg_chat_id, text="<b>Warning:</b> The message is too long to be sent",
+                    bot.send_message(chat_id=tg_chat_id, text="<b>تحذير:</b> الرسالة طويلة جداً ولا يمكن إرسالها",
                                      parse_mode=ParseMode.HTML)
         else:
             for link, title in zip(reversed(new_entry_links[-5:]), reversed(new_entry_titles[-5:])):
@@ -177,18 +167,17 @@ def rss_update(bot, job):
                 if len(final_message) <= constants.MAX_MESSAGE_LENGTH:
                     bot.send_message(chat_id=tg_chat_id, text=final_message, parse_mode=ParseMode.HTML)
                 else:
-                    bot.send_message(chat_id=tg_chat_id, text="<b>Warning:</b> The message is too long to be sent",
+                    bot.send_message(chat_id=tg_chat_id, text="<b>تحذير:</b> الرسالة طويلة جداً ولا يمكن إرسالها",
                                      parse_mode=ParseMode.HTML)
 
             bot.send_message(chat_id=tg_chat_id, parse_mode=ParseMode.HTML,
-                             text="<b>Warning: </b>{} occurrences have been left out to prevent spam"
+                             text="<b>تحذير: </b>{} تكراراً تم تجاهلها لمنع البريد المزعج"
                              .format(len(new_entry_links) - 5))
 
 
 def rss_set(bot, job):
     user_data = sql.get_all()
 
-    # this loop checks for every row in the DB
     for row in user_data:
         row_id = row.id
         tg_feed_link = row.feed_link
@@ -199,32 +188,34 @@ def rss_set(bot, job):
         new_entry_links = []
         new_entry_titles = []
 
-        # this loop checks for every entry from the RSS Feed link from the DB row
         for entry in feed_processed.entries:
-            # check if there are any new updates to the RSS Feed from the old entry
             if entry.link != tg_old_entry_link:
                 new_entry_links.append(entry.link)
                 new_entry_titles.append(entry.title)
             else:
                 break
 
-        # check if there's any new entries queued from the last check
         if new_entry_links:
             sql.update_url(row_id, new_entry_links)
-        else:
-            pass
 
 
+# ================== المساعدة ==================
 __help__ = """
- - /addrss <link>: add an RSS link to the subscriptions.
- - /removerss <link>: removes the RSS link from the subscriptions.
- - /rss <link>: shows the link's data and the last entry, for testing purposes.
- - /listrss: shows the list of rss feeds that the chat is currently subscribed to.
+- /addrss <رابط>: إضافة رابط RSS إلى الاشتراكات.
+- /removerss <رابط>: إزالة رابط RSS من الاشتراكات.
+- /rss <رابط>: عرض بيانات الرابط وآخر مدخلة، لأغراض الاختبار.
+- /listrss: عرض قائمة تغذيات RSS التي تشترك فيها الدردشة حالياً.
 
-NOTE: In groups, only admins can add/remove RSS links to the group's subscription
+ملاحظة: في المجموعات، يمكن للمشرفين فقط إضافة/إزالة روابط RSS إلى اشتراكات المجموعة.
+
+*الأوامر العربية (بدون /):*
+إضافة RSS <الرابط>: إضافة رابط RSS
+إزالة RSS <الرابط>: إزالة رابط RSS
+عرض RSS <الرابط>: عرض معلومات الرابط
+قائمة RSS: عرض قائمة الروابط المشترك بها
 """
 
-__mod_name__ = "RSS Feed"
+__mod_name__ = "تغذية RSS"
 
 job = updater.job_queue
 
@@ -238,7 +229,18 @@ ADD_URL_HANDLER = CommandHandler("addrss", add_url, pass_args=True)
 REMOVE_URL_HANDLER = CommandHandler("removerss", remove_url, pass_args=True)
 LIST_URLS_HANDLER = CommandHandler("listrss", list_urls)
 
+# معالجات الأوامر العربية
+SHOW_URL_AR_HANDLER = CommandHandler("عرض RSS", show_url, pass_args=True)
+ADD_URL_AR_HANDLER = CommandHandler("إضافة RSS", add_url, pass_args=True)
+REMOVE_URL_AR_HANDLER = CommandHandler("إزالة RSS", remove_url, pass_args=True)
+LIST_URLS_AR_HANDLER = CommandHandler("قائمة RSS", list_urls)
+
 dispatcher.add_handler(SHOW_URL_HANDLER)
 dispatcher.add_handler(ADD_URL_HANDLER)
 dispatcher.add_handler(REMOVE_URL_HANDLER)
 dispatcher.add_handler(LIST_URLS_HANDLER)
+
+dispatcher.add_handler(SHOW_URL_AR_HANDLER)
+dispatcher.add_handler(ADD_URL_AR_HANDLER)
+dispatcher.add_handler(REMOVE_URL_AR_HANDLER)
+dispatcher.add_handler(LIST_URLS_AR_HANDLER)
