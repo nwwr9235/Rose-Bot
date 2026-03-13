9b759338da5acb14f2b8cdbf9634846f3859564c
nwwr9235 <nwwr9235@gmail.com>
2026-03-13 04:18:02 +0300

    Update notes.py

--- tg_bot/modules/notes.py
+++ tg_bot/modules/notes.py
@@ -5,7 +5,7 @@ from typing import Optional, List
 from telegram import MAX_MESSAGE_LENGTH, ParseMode, InlineKeyboardMarkup
 from telegram import Message, Update, Bot
 from telegram.error import BadRequest
-from telegram.ext import CommandHandler, RegexHandler
+from telegram.ext import CommandHandler, RegexHandler, MessageHandler
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown
 
@@ -15,7 +15,6 @@ from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import user_admin
 from tg_bot.modules.helper_funcs.misc import build_keyboard, revert_buttons
 from tg_bot.modules.helper_funcs.msg_types import get_note_type
-
 from tg_bot.modules.connection import connected
 
 FILE_MATCHER = re.compile(r"^###file_id(!photo)?###:(.*?)(?:\s|$)")
@@ -32,11 +31,11 @@ ENUM_FUNC_MAP = {
 }
 
 
-# Do not async
+# لا تستخدم async
 def get(bot, update, notename, show_none=True, no_format=False):
     chat_id = update.effective_chat.id
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
+    chat = update.effective_chat
+    user = update.effective_user
     conn = connected(bot, update, chat, user.id, need_admin=False)
     if not conn == False:
         chat_id = conn
@@ -46,10 +45,9 @@ def get(bot, update, notename, show_none=True, no_format=False):
         send_id = chat_id
 
     note = sql.get_note(chat_id, notename)
-    message = update.effective_message  # type: Optional[Message]
+    message = update.effective_message
 
     if note:
-        # If we're replying to a message, reply to that message (unless it's an error)
         if message.reply_to_message:
             reply_id = message.reply_to_message.message_id
         else:
@@ -61,8 +59,7 @@ def get(bot, update, notename, show_none=True, no_format=False):
                     bot.forward_message(chat_id=update.effective_chat.id, from_chat_id=MESSAGE_DUMP, message_id=note.value)
                 except BadRequest as excp:
                     if excp.message == "Message to forward not found":
-                        message.reply_text("This message seems to have been lost - I'll remove it "
-                                           "from your notes list.")
+                        message.reply_text("يبدو أن هذه الرسالة قد فقدت - سأزيلها من قائمة ملاحظاتك.")
                         sql.rm_note(chat_id, notename)
                     else:
                         raise
@@ -71,10 +68,7 @@ def get(bot, update, notename, show_none=True, no_format=False):
                     bot.forward_message(chat_id=update.effective_chat.id, from_chat_id=chat_id, message_id=note.value)
                 except BadRequest as excp:
                     if excp.message == "Message to forward not found":
-                        message.reply_text("Looks like the original sender of this note has deleted "
-                                           "their message - sorry! Get your bot admin to start using a "
-                                           "message dump to avoid this. I'll remove this note from "
-                                           "your saved notes.")
+                        message.reply_text("يبدو أن المرسل الأصلي لهذه الملاحظة قد حذف رسالته - آسف! اطلب من مشرف البوت البدء في استخدام تفريغ الرسائل لتجنب هذا. سأزيل هذه الملاحظة من ملاحظاتك المحفوظة.")
                         sql.rm_note(chat_id, notename)
                     else:
                         raise
@@ -107,22 +101,17 @@ def get(bot, update, notename, show_none=True, no_format=False):
 
             except BadRequest as excp:
                 if excp.message == "Entity_mention_user_invalid":
-                    message.reply_text("Looks like you tried to mention someone I've never seen before. If you really "
-                                       "want to mention them, forward one of their messages to me, and I'll be able "
-                                       "to tag them!")
+                    message.reply_text("يبدو أنك حاولت ذكر شخص لم أره من قبل. إذا كنت تريد حقاً ذكره، قم بإعادة توجيه إحدى رسائله إلي، وسأتمكن من منشنهم!")
                 elif FILE_MATCHER.match(note.value):
-                    message.reply_text("This note was an incorrectly imported file from another bot - I can't use "
-                                       "it. If you really need it, you'll have to save it again. In "
-                                       "the meantime, I'll remove it from your notes list.")
+                    message.reply_text("تم استيراد هذه الملاحظة بشكل غير صحيح من بوت آخر - لا يمكنني استخدامها. إذا كنت بحاجة إليها حقاً، ستحتاج إلى حفظها مرة أخرى. في هذه الأثناء، سأزيلها من قائمة ملاحظاتك.")
                     sql.rm_note(chat_id, notename)
                 else:
-                    message.reply_text("This note could not be sent, as it is incorrectly formatted. Ask in "
-                                       "@keralabots if you can't figure out why!")
-                    LOGGER.exception("Could not parse message #%s in chat %s", notename, str(chat_id))
-                    LOGGER.warning("Message was: %s", str(note.value))
+                    message.reply_text("تعذر إرسال هذه الملاحظة، لأن تنسيقها غير صحيح. اسأل في @keralabots إذا كنت لا تستطيع معرفة السبب!")
+                    LOGGER.exception("تعذر تحليل الرسالة #%s في الدردشة %s", notename, str(chat_id))
+                    LOGGER.warning("الرسالة كانت: %s", str(note.value))
         return
     elif show_none:
-        message.reply_text("This note doesn't exist")
+        message.reply_text("هذه الملاحظة غير موجودة")
 
 
 @run_async
@@ -132,7 +121,7 @@ def cmd_get(bot: Bot, update: Update, args: List[str]):
     elif len(args) >= 1:
         get(bot, update, args[0], show_none=True)
     else:
-        update.effective_message.reply_text("Get rekt")
+        update.effective_message.reply_text("الاستخدام: /get <اسم_الملاحظة>")
 
 
 @run_async
@@ -146,8 +135,8 @@ def hash_get(bot: Bot, update: Update):
 @run_async
 @user_admin
 def save(bot: Bot, update: Update):
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
+    chat = update.effective_chat
+    user = update.effective_user
     conn = connected(bot, update, chat, user.id)
     if not conn == False:
         chat_id = conn
@@ -155,18 +144,17 @@ def save(bot: Bot, update: Update):
     else:
         chat_id = update.effective_chat.id
         if chat.type == "private":
-            chat_name = "local notes"
+            chat_name = "الملاحظات المحلية"
         else:
             chat_name = chat.title
 
-    msg = update.effective_message  # type: Optional[Message]
+    msg = update.effective_message
 
     note_name, text, data_type, content, buttons = get_note_type(msg)
 
     if data_type is None:
-        msg.reply_text("Dude, there's no note")
+        msg.reply_text("يا صاح، لا توجد ملاحظة")
         return
-
 
     if len(text.strip()) == 0:
         text = note_name
@@ -174,27 +162,21 @@ def save(bot: Bot, update: Update):
     sql.add_note_to_db(chat_id, note_name, text, data_type, buttons=buttons, file=content)
 
     msg.reply_text(
-        "OK, Added {note_name} in *{chat_name}*.\nGet it with /get {note_name}, or #{note_name}".format(note_name=note_name, chat_name=chat_name), parse_mode=ParseMode.MARKDOWN)
+        "تمت إضافة {} في *{}*.\nيمكنك الحصول عليها بـ /get {}، أو #{}".format(note_name, chat_name, note_name, note_name), parse_mode=ParseMode.MARKDOWN)
 
     if msg.reply_to_message and msg.reply_to_message.from_user.is_bot:
         if text:
-            msg.reply_text("Seems like you're trying to save a message from a bot. Unfortunately, "
-                           "bots can't forward bot messages, so I can't save the exact message. "
-                           "\nI'll save all the text I can, but if you want more, you'll have to "
-                           "forward the message yourself, and then save it.")
+            msg.reply_text("يبدو أنك تحاول حفظ رسالة من بوت. لسوء الحظ، لا يمكن للبوتات إعادة توجيه رسائل البوتات، لذا لا يمكنني حفظ الرسالة بالضبط. سأحفظ كل النص الذي يمكنني، ولكن إذا كنت تريد المزيد، فسيتعين عليك إعادة توجيه الرسالة بنفسك، ثم حفظها.")
         else:
-            msg.reply_text("Bots are kinda handicapped by telegram, making it hard for bots to "
-                           "interact with other bots, so I can't save this message "
-                           "like I usually would - do you mind forwarding it and "
-                           "then saving that new message? Thanks!")
+            msg.reply_text("البوتات مقيدة نوعاً ما بواسطة تليجرام، مما يجعل من الصعب على البوتات التفاعل مع البوتات الأخرى، لذا لا يمكنني حفظ هذه الرسالة كما أفعل عادةً - هل تمانع في إعادة توجيهها ثم حفظ تلك الرسالة الجديدة؟ شكراً!")
         return
 
 
 @run_async
 @user_admin
 def clear(bot: Bot, update: Update, args: List[str]):
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
+    chat = update.effective_chat
+    user = update.effective_user
     conn = connected(bot, update, chat, user.id)
     if not conn == False:
         chat_id = conn
@@ -202,7 +184,7 @@ def clear(bot: Bot, update: Update, args: List[str]):
     else:
         chat_id = update.effective_chat.id
         if chat.type == "private":
-            chat_name = "local notes"
+            chat_name = "الملاحظات المحلية"
         else:
             chat_name = chat.title
 
@@ -210,29 +192,29 @@ def clear(bot: Bot, update: Update, args: List[str]):
         notename = args[0]
 
         if sql.rm_note(chat_id, notename):
-            update.effective_message.reply_text("Successfully removed note.")
+            update.effective_message.reply_text("تمت إزالة الملاحظة بنجاح.")
         else:
-            update.effective_message.reply_text("That's not a note in my database!")
+            update.effective_message.reply_text("هذه ليست ملاحظة في قاعدة البيانات!")
 
 
 @run_async
 def list_notes(bot: Bot, update: Update):
     chat_id = update.effective_chat.id
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
+    chat = update.effective_chat
+    user = update.effective_user
     conn = connected(bot, update, chat, user.id, need_admin=False)
     if not conn == False:
         chat_id = conn
         chat_name = dispatcher.bot.getChat(conn).title
-        msg = "*Notes in {}:*\n".format(chat_name)
+        msg = "*الملاحظات في {}:*\n".format(chat_name)
     else:
         chat_id = update.effective_chat.id
         if chat.type == "private":
             chat_name = ""
-            msg = "*Local Notes:*\n"
+            msg = "*الملاحظات المحلية:*\n"
         else:
             chat_name = chat.title
-            msg = "*Notes in {}:*\n".format(chat_name)
+            msg = "*الملاحظات في {}:*\n".format(chat_name)
 
     note_list = sql.get_all_chat_notes(chat_id)
 
@@ -243,8 +225,8 @@ def list_notes(bot: Bot, update: Update):
             msg = ""
         msg += note_name
 
-    if msg == "*Notes in chat:*\n":
-        update.effective_message.reply_text("No notes in this chat!")
+    if msg == "*الملاحظات في الدردشة:*\n":
+        update.effective_message.reply_text("لا توجد ملاحظات في هذه الدردشة!")
 
     elif len(msg) != 0:
         update.effective_message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
@@ -267,13 +249,11 @@ def __import_data__(chat_id, data):
         with BytesIO(str.encode("\n".join(failures))) as output:
             output.name = "failed_imports.txt"
             dispatcher.bot.send_document(chat_id, document=output, filename="failed_imports.txt",
-                                         caption="These files/photos failed to import due to originating "
-                                                 "from another bot. This is a telegram API restriction, and can't "
-                                                 "be avoided. Sorry for the inconvenience!")
+                                         caption="فشلت استيراد هذه الملفات/الصور بسبب مصدرها من بوت آخر. هذا قيد من واجهة برمجة تطبيقات تليجرام، ولا يمكن تجنبه. نأسف للإزعاج!")
 
 
 def __stats__():
-    return "{} notes, across {} chats.".format(sql.num_notes(), sql.num_chats())
+    return "{} ملاحظة، عبر {} دردشة.".format(sql.num_notes(), sql.num_chats())
 
 
 def __migrate__(old_chat_id, new_chat_id):
@@ -282,26 +262,31 @@ def __migrate__(old_chat_id, new_chat_id):
 
 def __chat_settings__(chat_id, user_id):
     notes = sql.get_all_chat_notes(chat_id)
-    return "There are `{}` notes in this chat.".format(len(notes))
+    return "هناك `{}` ملاحظة في هذه الدردشة.".format(len(notes))
 
 
+# ================== المساعدة ==================
 __help__ = """
- - /get <notename>: get the note with this notename
- - #<notename>: same as /get
- - /notes or /saved: list all saved notes in this chat
+- /get <اسم_الملاحظة>: الحصول على الملاحظة بهذا الاسم
+- #<اسم_الملاحظة>: نفس /get
+- /notes أو /saved: قائمة بجميع الملاحظات المحفوظة في هذه الدردشة
 
-If you would like to retrieve the contents of a note without any formatting, use `/get <notename> noformat`. This can \
-be useful when updating a current note.
+إذا أردت استرجاع محتويات ملاحظة بدون أي تنسيق، استخدم `/get <اسم_الملاحظة> noformat`. يمكن أن يكون هذا مفيداً عند تحديث ملاحظة حالية.
 
-*Admin only:*
- - /save <notename> <notedata>: saves notedata as a note with name notename
-A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a \
-`buttonurl:` section, as such: `[somelink](buttonurl:example.com)`. Check /markdownhelp for more info.
- - /save <notename>: save the replied message as a note with name notename
- - /clear <notename>: clear note with this name
+*للمشرفين فقط:*
+- /save <اسم_الملاحظة> <بيانات_الملاحظة>: يحفظ بيانات_الملاحظة كملاحظة باسم اسم_الملاحظة
+يمكن إضافة زر إلى ملاحظة باستخدام صيغة رابط ماركداون القياسية - يجب أن يسبق الرابط قسم `buttonurl:`، كما يلي: `[رابط ما](buttonurl:example.com)`. تحقق من /markdownhelp لمزيد من المعلومات.
+- /save <اسم_الملاحظة>: حفظ الرسالة التي تم الرد عليها كملاحظة باسم اسم_الملاحظة
+- /clear <اسم_الملاحظة>: مسح الملاحظة بهذا الاسم
+
+*الأوامر العربية (بدون /):*
+ملاحظة <الاسم>: الحصول على ملاحظة
+ملاحظات: عرض قائمة الملاحظات
+حفظ ملاحظة <الاسم> <النص>: حفظ ملاحظة جديدة
+حذف ملاحظة <الاسم>: حذف ملاحظة
 """
 
-__mod_name__ = "Notes"
+__mod_name__ = "الملاحظات"
 
 GET_HANDLER = CommandHandler("get", cmd_get, pass_args=True)
 HASH_GET_HANDLER = RegexHandler(r"^#[^\s]+", hash_get)
@@ -311,8 +296,19 @@ DELETE_HANDLER = CommandHandler("clear", clear, pass_args=True)
 
 LIST_HANDLER = DisableAbleCommandHandler(["notes", "saved"], list_notes, admin_ok=True)
 
+# معالجات الأوامر العربية
+GET_AR_HANDLER = CommandHandler("ملاحظة", cmd_get, pass_args=True)
+LIST_AR_HANDLER = CommandHandler("ملاحظات", list_notes)
+SAVE_AR_HANDLER = CommandHandler("حفظ ملاحظة", save)
+DELETE_AR_HANDLER = CommandHandler("حذف ملاحظة", clear, pass_args=True)
+
 dispatcher.add_handler(GET_HANDLER)
 dispatcher.add_handler(SAVE_HANDLER)
 dispatcher.add_handler(LIST_HANDLER)
 dispatcher.add_handler(DELETE_HANDLER)
 dispatcher.add_handler(HASH_GET_HANDLER)
+
+dispatcher.add_handler(GET_AR_HANDLER)
+dispatcher.add_handler(LIST_AR_HANDLER)
+dispatcher.add_handler(SAVE_AR_HANDLER)
+dispatcher.add_handler(DELETE_AR_HANDLER)
