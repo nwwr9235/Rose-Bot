0e91c37025a8859c2b92c5d1d0218f27baec9c0a
nwwr9235 <nwwr9235@gmail.com>
2026-03-13 04:38:48 +0300

    Update cust_filters.py

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -20,35 +20,35 @@ from tg_bot.modules.sql import cust_filters_sql as sql
 from tg_bot.modules.connection import connected
 
 HANDLER_GROUP = 15
-BASIC_FILTER_STRING = "*Filters in this chat:*\n"
+BASIC_FILTER_STRING = "*عوامل التصفية في هذه الدردشة:*\n"
 
 
 @run_async
 def list_handlers(bot: Bot, update: Update):
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
+    chat = update.effective_chat
+    user = update.effective_user
 
     conn = connected(bot, update, chat, user.id, need_admin=False)
     if not conn == False:
         chat_id = conn
         chat_name = dispatcher.bot.getChat(conn).title
-        filter_list = f"*Filters in {chat_name}:*\n"
+        filter_list = f"*عوامل التصفية في {chat_name}:*\n"
     else:
         chat_id = update.effective_chat.id
         if chat.type == "private":
-            chat_name = "local filters"
-            filter_list = "*local filters:*\n"
+            chat_name = "عوامل التصفية المحلية"
+            filter_list = "*عوامل التصفية المحلية:*\n"
         else:
             chat_name = chat.title
-            filter_list = "*Filters in {}*:\n".format(chat_name)
+            filter_list = "*عوامل التصفية في {}*:\n".format(chat_name)
 
     total_count_f_fliters = sql.num_filters_per_chat(chat_id)
-    filter_list += f"**Filter Count**: {total_count_f_fliters}\n"
+    filter_list += f"**عدد عوامل التصفية**: {total_count_f_fliters}\n"
 
     all_handlers = sql.get_chat_triggers(chat_id)
 
     if not all_handlers:
-        update.effective_message.reply_text("No filters in {}!".format(chat_name))
+        update.effective_message.reply_text("لا توجد عوامل تصفية في {}!".format(chat_name))
         return
 
     for keyword in all_handlers:
@@ -66,10 +66,10 @@ def list_handlers(bot: Bot, update: Update):
 # NOT ASYNC BECAUSE DISPATCHER HANDLER RAISED
 @user_admin
 def filters(bot: Bot, update: Update):
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
-    msg = update.effective_message  # type: Optional[Message]
-    args = msg.text.split(None, 1)  # use python's maxsplit to separate Cmd, keyword, and reply_text
+    chat = update.effective_chat
+    user = update.effective_user
+    msg = update.effective_message
+    args = msg.text.split(None, 1)
 
     conn = connected(bot, update, chat, user.id)
     if not conn == False:
@@ -78,7 +78,7 @@ def filters(bot: Bot, update: Update):
     else:
         chat_id = update.effective_chat.id
         if chat.type == "private":
-            chat_name = "local filters"
+            chat_name = "عوامل التصفية المحلية"
         else:
             chat_name = chat.title
 
@@ -90,18 +90,16 @@ def filters(bot: Bot, update: Update):
         total_fs = sql.num_filters_per_chat(chat_id)
         if total_fs >= BMERNU_SCUT_SRELFTI:
             msg.reply_text(
-                f"You currently have {total_fs} filters. "
-                f"The maximum number of filters allowed is {BMERNU_SCUT_SRELFTI}. "
-                "You need to delete some filters "
-                "before being allowed to add more "
-                "or use @kochufilterbot for unlimited filters."
+                f"لديك حالياً {total_fs} عامل تصفية. "
+                f"الحد الأقصى المسموح به هو {BMERNU_SCUT_SRELFTI}. "
+                "يجب حذف بعض عوامل التصفية قبل إضافة المزيد "
+                "أو استخدم @kochufilterbot لعوامل تصفية غير محدودة."
             )
             return
 
     extracted = split_quotes(args[1])
     if len(extracted) < 1:
         return
-    # set trigger -> lower, so as to avoid adding duplicate filters with different cases
     keyword = extracted[0].lower()
 
     is_sticker = False
@@ -112,20 +110,17 @@ def filters(bot: Bot, update: Update):
     is_video = False
     media_caption = None
     has_caption = False
-    content = None # :\
+    content = None
     buttons = []
 
-    # determine what the contents of the filter are - text, image, sticker, etc
     if len(extracted) >= 2:
-        offset = len(extracted[1]) - len(msg.text)  # set correct offset relative to command + notename
+        offset = len(extracted[1]) - len(msg.text)
         content, buttons = button_markdown_parser(extracted[1], entities=msg.parse_entities(), offset=offset)
         content = content.strip()
-        # https://t.me/c/1279877202/3508
 
     if msg.reply_to_message and msg.reply_to_message.sticker:
         content = msg.reply_to_message.sticker.file_id
         is_sticker = True
-        # stickers don't have caption in BOT API -_-
 
     elif msg.reply_to_message and msg.reply_to_message.document:
         offset = len(msg.reply_to_message.caption or "")
@@ -137,7 +132,7 @@ def filters(bot: Bot, update: Update):
     elif msg.reply_to_message and msg.reply_to_message.photo:
         offset = len(msg.reply_to_message.caption or "")
         media_caption, buttons = button_markdown_parser(msg.reply_to_message.caption, entities=msg.reply_to_message.parse_entities(), offset=offset)
-        content = msg.reply_to_message.photo[-1].file_id  # last elem = best quality
+        content = msg.reply_to_message.photo[-1].file_id
         is_image = True
         has_caption = True
 
@@ -166,12 +161,9 @@ def filters(bot: Bot, update: Update):
         content = msg.reply_to_message.text
 
     elif not content:
-        # msg.reply_text("You didn't specify what to reply with!")
-        msg.reply_text("There is no note message - You can't JUST have buttons, you need a message to go with it!")
+        msg.reply_text("لا توجد رسالة ملاحظة - لا يمكن أن يكون لديك أزرار فقط، تحتاج إلى رسالة معها!")
         return
 
-    # Add the filter
-    # Note: perhaps handlers can be removed somehow using sql.get_chat_filters
     for handler in dispatcher.handlers.get(HANDLER_GROUP, []):
         if handler.filters == (keyword, chat.id):
             dispatcher.remove_handler(handler, HANDLER_GROUP)
@@ -179,15 +171,15 @@ def filters(bot: Bot, update: Update):
     sql.add_filter(chat_id, keyword, content, is_sticker, is_document, is_image, is_audio, is_voice, is_video,
                    buttons, media_caption, has_caption)
 
-    msg.reply_text("Handler '{}' added in *{}*!".format(keyword, chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
+    msg.reply_text("تمت إضافة عامل التصفية '{}' في *{}*!".format(keyword, chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
     raise DispatcherHandlerStop
 
 
 # NOT ASYNC BECAUSE DISPATCHER HANDLER RAISED
 @user_admin
 def stop_filter(bot: Bot, update: Update):
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
+    chat = update.effective_chat
+    user = update.effective_user
     args = update.effective_message.text.split(None, 1)
 
     conn = connected(bot, update, chat, user.id)
@@ -197,7 +189,7 @@ def stop_filter(bot: Bot, update: Update):
     else:
         chat_id = chat.id
         if chat.type == "private":
-            chat_name = "local notes"
+            chat_name = "الملاحظات المحلية"
         else:
             chat_name = chat.title
 
@@ -207,29 +199,28 @@ def stop_filter(bot: Bot, update: Update):
     chat_filters = sql.get_chat_triggers(chat_id)
 
     if not chat_filters:
-        update.effective_message.reply_text("No filters are active here!")
+        update.effective_message.reply_text("لا توجد عوامل تصفية نشطة هنا!")
         return
 
     for keyword in chat_filters:
         if keyword == args[1]:
             sql.remove_filter(chat_id, args[1])
-            update.effective_message.reply_text("Yep, I'll stop replying to that in *{}*.".format(chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
+            update.effective_message.reply_text("تم إيقاف الرد على ذلك في *{}*.".format(chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
             raise DispatcherHandlerStop
 
-    update.effective_message.reply_text("That's not a current filter - run /filters for all active filters.")
+    update.effective_message.reply_text("هذا ليس عامل تصفية حالياً - استخدم /filters لعرض جميع عوامل التصفية النشطة.")
 
 
 @run_async
 def reply_filter(bot: Bot, update: Update):
-    chat = update.effective_chat  # type: Optional[Chat]
-    message = update.effective_message  # type: Optional[Message]
+    chat = update.effective_chat
+    message = update.effective_message
     to_match = extract_text(message)
     if not to_match:
         return
 
     if message.reply_to_message:
         message = message.reply_to_message
-
 
     chat_filters = sql.get_chat_triggers(chat.id)
     for keyword in chat_filters:
@@ -287,7 +278,6 @@ def reply_filter(bot: Bot, update: Update):
                     reply_markup=keyboard,
                     api_kwargs={"allow_sending_without_reply": True}
                 )
-
             elif filt.has_markdown:
                 keyb = build_keyboard(buttons)
                 keyboard = InlineKeyboardMarkup(keyb)
@@ -302,23 +292,19 @@ def reply_filter(bot: Bot, update: Update):
                                        reply_markup=keyboard)
                 except BadRequest as excp:
                     if excp.message == "Unsupported url protocol":
-                        message.reply_text("You seem to be trying to use an unsupported url protocol. Telegram "
-                                           "doesn't support buttons for some protocols, such as tg://. Please try "
-                                           "again, or ask in @KeralaBots for help.")
+                        message.reply_text("يبدو أنك تحاول استخدام بروتوكول رابط غير مدعوم. تليجرام لا يدعم الأزرار لبعض البروتوكولات، مثل tg://. يرجى المحاولة مرة أخرى، أو اسأل في @KeralaBots للمساعدة.")
                     elif excp.message == "Replied message not found":
                         bot.send_message(chat.id, filt.reply, parse_mode=ParseMode.MARKDOWN,
                                          disable_web_page_preview=True,
                                          reply_markup=keyboard)
                     else:
-                        message.reply_text("This note could not be sent, as it is incorrectly formatted. Ask in "
-                                           "@KeralaBots if you can't figure out why!")
-                        LOGGER.warning("Message %s could not be parsed", str(filt.reply))
-                        LOGGER.exception("Could not parse filter %s in chat %s", str(filt.keyword), str(chat.id))
-
+                        message.reply_text("تعذر إرسال هذه الملاحظة، لأن تنسيقها غير صحيح. اسأل في @KeralaBots إذا كنت لا تستطيع معرفة السبب!")
+                        LOGGER.warning("الرسالة %s تعذر تحليلها", str(filt.reply))
+                        LOGGER.exception("تعذر تحليل عامل التصفية %s في الدردشة %s", str(filt.keyword), str(chat.id))
             else:
-                # LEGACY - all new filters will have has_markdown set to True.
                 message.reply_text(filt.reply)
             break
+
 
 @run_async
 def rmall_filters(bot: Bot, update: Update):
@@ -326,13 +312,12 @@ def rmall_filters(bot: Bot, update: Update):
     user = update.effective_user
     member = chat.get_member(user.id)
     if member.status != "creator" and user.id not in SUDO_USERS:
-        update.effective_message.reply_text(
-            "Only the chat owner can clear all notes at once.")
+        update.effective_message.reply_text("فقط منشئ الدردشة يمكنه مسح جميع عوامل التصفية دفعة واحدة.")
     else:
-        buttons = InlineKeyboardMarkup([[InlineKeyboardButton(text="Stop all filters", callback_data="filters_rmall")], [
-                                       InlineKeyboardButton(text="Cancel", callback_data="filters_cancel")]])
+        buttons = InlineKeyboardMarkup([[InlineKeyboardButton(text="إيقاف جميع عوامل التصفية", callback_data="filters_rmall")], [
+                                       InlineKeyboardButton(text="إلغاء", callback_data="filters_cancel")]])
         update.effective_message.reply_text(
-            f"Are you sure you would like to stop ALL filters in {chat.title}? This action cannot be undone.", reply_markup=buttons, parse_mode=ParseMode.MARKDOWN)
+            f"هل أنت متأكد أنك تريد إيقاف جميع عوامل التصفية في {chat.title}؟ لا يمكن التراجع عن هذا الإجراء.", reply_markup=buttons, parse_mode=ParseMode.MARKDOWN)
 
 
 @run_async
@@ -345,7 +330,7 @@ def rmall_callback(bot: Bot, update: Update):
         if member.status == "creator" or query.from_user.id in SUDO_USERS:
             allfilters = sql.get_chat_triggers(chat.id)
             if not allfilters:
-                msg.edit_text("No filters in this chat, nothing to stop!")
+                msg.edit_text("لا توجد عوامل تصفية في هذه الدردشة، لا شيء لإيقافه!")
                 return
 
             count = 0
@@ -357,29 +342,25 @@ def rmall_callback(bot: Bot, update: Update):
             for i in filterlist:
                 sql.remove_filter(chat.id, i)
 
-            msg.edit_text(f"Cleaned {count} filters in {chat.title}")
+            msg.edit_text(f"تم تنظيف {count} عامل تصفية في {chat.title}")
 
         if member.status == "administrator":
-            query.answer(
-                "Only owner of the chat can do this.")
+            query.answer("فقط منشئ الدردشة يمكنه القيام بذلك.")
 
         if member.status == "member":
-            query.answer(
-                "You need to be admin to do this.")
+            query.answer("يجب أن تكون مشرفاً للقيام بذلك.")
     elif query.data == 'filters_cancel':
         if member.status == "creator" or query.from_user.id in SUDO_USERS:
-            msg.edit_text("Clearing of all filters has been cancelled.")
+            msg.edit_text("تم إلغاء مسح جميع عوامل التصفية.")
             return
         if member.status == "administrator":
-            query.answer(
-                "Only owner of the chat can do this.")
+            query.answer("فقط منشئ الدردشة يمكنه القيام بذلك.")
         if member.status == "member":
-            query.answer(
-                "You need to be admin to do this.")
+            query.answer("يجب أن تكون مشرفاً للقيام بذلك.")
 
 
 def __stats__():
-    return "{} filters, across {} chats.".format(sql.num_filters(), sql.num_chats())
+    return "{} عامل تصفية، عبر {} دردشة.".format(sql.num_filters(), sql.num_chats())
 
 
 def __migrate__(old_chat_id, new_chat_id):
@@ -388,24 +369,27 @@ def __migrate__(old_chat_id, new_chat_id):
 
 def __chat_settings__(chat_id, user_id):
     cust_filters = sql.get_chat_triggers(chat_id)
-    return "There are `{}` custom filters here.".format(len(cust_filters))
+    return "هناك `{}` عامل تصفية مخصص هنا.".format(len(cust_filters))
 
 
+# ================== المساعدة ==================
 __help__ = """
- - /filters: list all active filters in this chat.
+- /filters: قائمة بجميع عوامل التصفية النشطة في هذه الدردشة.
 
-*Admin only:*
- - /filter <keyword> <reply message>: add a filter to this chat. The bot will now reply that message whenever 'keyword'\
-is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
-keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
-doin?
- - /stop <filter keyword>: stop that filter.
-*Chat creator only:*
- - /removeallfilters: Stop all filters in chat at once (Limited to creators only).
+*للمشرفين فقط:*
+- /filter <كلمة مفتاحية> <رسالة الرد>: إضافة عامل تصفية إلى هذه الدردشة. سيقوم البوت الآن بالرد بتلك الرسالة كلما تم ذكر 'الكلمة المفتاحية'. إذا قمت بالرد على ملصق بكلمة مفتاحية، سيرد البوت بذلك الملصق. ملاحظة: جميع الكلمات المفتاحية لعوامل التصفية تكون بأحرف صغيرة. إذا أردت أن تكون كلمتك المفتاحية جملة، استخدم علامات الاقتباس. مثال: /filter "مرحباً" كيف حالك؟
+- /stop <كلمة مفتاحية>: إيقاف عامل التصفية هذا.
+*لمنشئ الدردشة فقط:*
+- /removeallfilters: إيقاف جميع عوامل التصفية في الدردشة دفعة واحدة (مقتصر على المنشئين فقط).
 
+*الأوامر العربية (بدون /):*
+عوامل التصفية: عرض جميع عوامل التصفية
+إضافة عامل تصفية <كلمة> <الرد>: إضافة عامل تصفية جديد
+إيقاف عامل تصفية <كلمة>: إيقاف عامل تصفية معين
+إزالة كل عوامل التصفية: إزالة جميع عوامل التصفية (للمنشئ فقط)
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "عوامل التصفية"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
@@ -414,9 +398,20 @@ RMALLFILTER_CALLBACK = CallbackQueryHandler(rmall_callback, pattern=r"filters_.*
 LIST_HANDLER = DisableAbleCommandHandler("filters", list_handlers, admin_ok=True)
 CUST_FILTER_HANDLER = MessageHandler(CustomFilters.has_text, reply_filter)
 
+# معالجات الأوامر العربية
+LIST_AR_HANDLER = CommandHandler("عوامل التصفية", list_handlers)
+FILTER_AR_HANDLER = CommandHandler("إضافة عامل تصفية", filters)
+STOP_AR_HANDLER = CommandHandler("إيقاف عامل تصفية", stop_filter)
+RMALL_AR_HANDLER = CommandHandler("إزالة كل عوامل التصفية", rmall_filters, filters=Filters.group)
+
 dispatcher.add_handler(FILTER_HANDLER)
 dispatcher.add_handler(STOP_HANDLER)
 dispatcher.add_handler(RMALLFILTER_HANDLER)
 dispatcher.add_handler(RMALLFILTER_CALLBACK)
 dispatcher.add_handler(LIST_HANDLER)
 dispatcher.add_handler(CUST_FILTER_HANDLER, HANDLER_GROUP)
+
+dispatcher.add_handler(LIST_AR_HANDLER)
+dispatcher.add_handler(FILTER_AR_HANDLER)
+dispatcher.add_handler(STOP_AR_HANDLER)
+dispatcher.add_handler(RMALL_AR_HANDLER)
